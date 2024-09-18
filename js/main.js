7b1a94c5564ac612f6db7d995f3ea297760584d4
sjy8336 <sjy8336@gmail.com>
2024-09-18 21:24:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const mvS = new Swiper('.mv_in', {
+    loop: true,
+    autoplay:{delay:2500},
+    scrollbar: {el: '.mv_in .swiper-scrollbar'},
+});
+const bestS = new Swiper('.best_s', {
+    
+    scrollbar: {el: '.mv_in .swiper-scrollbar'},
+});
