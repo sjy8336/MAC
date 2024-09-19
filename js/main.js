805c7efcf2ebc54eb9cfbfee6108cbb3a29811dc
sjy8336 <sjy8336@gmail.com>
2024-09-19 11:05:27 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ const mvS = new Swiper('.mv_in', {
     scrollbar: {el: '.mv_in .swiper-scrollbar'},
 });
 const bestS = new Swiper('.best_s', {
-    
-    scrollbar: {el: '.mv_in .swiper-scrollbar'},
+    slidesPerView: 5,
+    spaceBetween: 40,
+    navigation:{
+        prevEl:'.best .swiper-button-prev',
+        nextEl:'.best .swiper-button-next',
+    },
+    scrollbar: {el: '.best .swiper-scrollbar'},
+});
+const newS = new Swiper('.new_s', {
+    loop: true,
+    slidesPerView: 1,
+    navigation:{
+        prevEl:'.new_in .swiper-button-prev',
+        nextEl:'.new_in .swiper-button-next',
+    },
 });
