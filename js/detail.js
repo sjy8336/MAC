e4b9fd9c6822164203f9d0c86aa624030be90012
sjy8336 <sjy8336@gmail.com>
2024-10-07 15:54:05 +0900

    Add files via upload

---
+++ js/detail.js
@@ -0,0 +1,7 @@
+const buyS = new Swiper('.buy_slide', {
+    loop: true,
+    navigation:{
+        prevEl:'.buy_slide .swiper-button-prev',
+        nextEl:'.buy_slide .swiper-button-next',
+    },
+});
